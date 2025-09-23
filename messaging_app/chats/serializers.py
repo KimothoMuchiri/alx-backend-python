8a99d6129271baaef8587ee7bfb155c30b0e0d19
KimothoMuchiri <muchirikimotho16@gmail.com>
2025-09-24 00:05:03 +0300

    serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -15,9 +15,14 @@ class MessageSerializer(serializers.ModelSerializer):
         fields = ['message_id', 'sender_id', 'message_body', 'sent_at']
 
 class ConversationSerializer(serializers.ModelSerializer):
-    messages = MessagerSerializer(many = True, read_only = True)
-    participants = UserSerializer(many = True, read_only = True)
+    messages = serializers.SerializerMethodField()
+    participants_id = UserSerializer(many = True, read_only = True)
 
     class Meta:
         model = Conversation
         fields = ['conversation_id','participants_id','created_at','messages']
+
+    def get_messages(self, obj):
+        messages_for_this_conversation = obj.message_set.all()
+        serializer = MessageSerializer(messages_for_this_conversation, many=True)
+        return serializer.data

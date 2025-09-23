4ab59a7d5e946e9045d8ea67b6e19ffac195e2e6
KimothoMuchiri <muchirikimotho16@gmail.com>
2025-09-23 09:08:34 +0300

    django rest

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'chats',
+    'rest_framework', # Django REST Framework
+    'corsheaders', # CORS headers
+    'drf_yasg', # Swagger documentation
 ]
 
 MIDDLEWARE = [

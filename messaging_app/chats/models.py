a5e2815aa48e24c40f15d772c9a43b3eeda86dce
KimothoMuchiri <muchirikimotho16@gmail.com>
2025-09-24 09:09:04 +0300

    serializers correction

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,5 +1,5 @@
 import uuid
-from django.db import models(
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
@@ -16,9 +16,23 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     role = models.CharField(max_length = 5, choices= ROLE_CHOICES, default = 'guest', null = False)
     created_at = models.DateTimeField(auto_now_add=True)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='chats_users_groups', # <--- Add this line
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_query_name='user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='chats_users_permissions', # <--- Add this line
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_query_name='user',
+    )
 
 class Conversation(models.Model):
-    conversation_id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
+    conversation_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     participants = models.ManyToManyField('chats.User')
     created_at = models.DateTimeField(auto_now_add= True)
     
